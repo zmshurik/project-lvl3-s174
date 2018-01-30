2eb4434cd515a66ca9feda51a7830489e6522559
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-30 19:15:04 +0700

    add pages templates

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('home', 'active')
+
+@section('content')
+<div class="jumbotron">   
+  <p class="lead">Enter URL and press send</p>
+  <hr class="my-4">
+  <form action="/domains" method="post">        
+        <input type="text" name="url">
+        <input class="btn btn-primary" type="submit" value="Send">
+    </form>
+</div>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,24 +7,21 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>My app</title>        
+    <title>Page analyzer</title>        
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <span class="navbar-brand">Page analyzer</span>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home</a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      <a class="nav-item nav-link @yield('home')" href="/">Home</a>
     </div>
   </div>
 </nav>
-
+@yield('content')
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
