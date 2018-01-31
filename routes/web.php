c64e2ccf99bb2c6c4ca42b022c88060cb4ac56fa
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-31 20:28:11 +0700

    add routs

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,28 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use App\Domain;
 
 $router->get('/', function () use ($router) {
-    return view('main');
+    return view('home');
 });
+
+$router->post('/domains', function (Illuminate\Http\Request $request) use ($router) {
+    $validator = \Validator::make($request->all(), [
+        'url' => 'required|url',
+        ]);
+
+
+    if ($validator->fails()) {
+        return view('home', ['errors' => $validator->errors()->all()]);
+    }
+
+    $domain = Domain::updateOrCreate(['name' => $request['url']]);
+    $id = $domain->id;
+    return redirect()->route('domain', ['id' => $id]);
+});
+
+$router->get('/domains/{id}', ['as' => 'domain', function ($id) use ($router) {
+    $domain = Domain::find($id);
+    return view('domains', ["domain" => $domain]);
+}]);
