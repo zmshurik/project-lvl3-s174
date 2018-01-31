52e5696c91f3bd105b0a295e1ef4b438dfd3505a
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-31 18:36:34 +0700

    uncomment facades eloquent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,9 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -66,7 +66,6 @@ $app->singleton(
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
-
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
