89e1f3b626879220fd9a59a994df3252e79c8782
Alexander Kurakov <zmshurik@yandex.ru>
2018-01-31 18:37:28 +0700

    add pages blades

---
+++ resources/views/domains.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('home', '')
+
+@section('content')
+  <p class="lead">{{ print_r($url) }}</p>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,19 @@
 @section('home', 'active')
 
 @section('content')
-<div class="jumbotron">   
+<div class="jumbotron" align="center">
   <p class="lead">Enter URL and press send</p>
   <hr class="my-4">
   <form action="/domains" method="post">        
         <input type="text" name="url">
         <input class="btn btn-primary" type="submit" value="Send">
-    </form>
+  </form>
+  @isset($errors)
+    @foreach ($errors as $error)
+        @component('layouts.alert')
+            {{ $error }}
+        @endcomponent
+    @endforeach
+@endisset
 </div>
 @endsection
