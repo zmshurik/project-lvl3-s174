7c5125c87b68a3aab1fd66e21f3d0c2b2f6a3880
Alexander Kurakov <zmshurik@yandex.ru>
2018-02-03 11:27:45 +0700

    change test

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -2,6 +2,11 @@
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
 
 class AppTest extends TestCase
 {
@@ -11,6 +16,7 @@ class AppTest extends TestCase
      *
      * @return void
      */
+
     public function testMainPage()
     {
         $this->get('/');
@@ -19,15 +25,27 @@ class AppTest extends TestCase
 
     public function testDomainsPage()
     {
-        DB::insert('insert into domains (name) values (?)', ['domain1']);
-        DB::insert('insert into domains (name) values (?)', ['domain2']);        
+        DB::insert('insert into domains (name, page_body, content_length, response_code) values (?, ?, ?, ?)', ['domain1', 'body', 4, 200]);
+        DB::insert('insert into domains (name, page_body, content_length, response_code) values (?, ?, ?, ?)', ['domain2', 'body2', 5, 200]);
         $this->get('/domains');
         $this->assertResponseOk();
     }
 
     public function testDatabaseCreateRaw()
     {
-        $this->post('/domains', ['url' => 'http://domain.com']);
-        $this->seeInDatabase('domains', ['name' => 'domain.com']);
+        $mock = new MockHandler([
+            new Response(200, ['Content-Length' => 4], 'body')
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        $this->app->instance(\GuzzleHttp\Client::class, $client);
+            
+        $this->post('/domains', ['url' => 'http://domain.com']);       
+        $this->seeInDatabase('domains', [
+            'name' => 'domain.com',
+            'page_body' => 'body',
+            'content_length' => 4,
+            'response_code' => 200
+        ]);
     }
 }
