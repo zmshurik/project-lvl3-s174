a37c98a778dc40a5c476bb49fcd736cf13aa5d40
Alexander Kurakov <zmshurik@yandex.ru>
2018-02-03 11:39:44 +0700

    add guzzle requests

--- app/Domains.php
+++ app/Domains.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Domains extends Model
 {
-    protected $fillable = array('name');
+    protected $fillable = array('name', 'page_body', 'response_code', 'content_length');
     /**
      * The table associated with the model.
      *
--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use \App\Domains;
+use \GuzzleHttp\Client;
 
 class DomainController extends Controller
 {
+    private $guzzleClient;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Client $client)
     {
-        //
+        $this->$guzzleClient = $client;
     }
 
     public function index()
@@ -32,15 +34,31 @@ class DomainController extends Controller
     {
         $validator = \Validator::make($request->all(), [
             'url' => 'required|url',
-            ]);
-
+        ]);
 
         if ($validator->fails()) {
             return view('home', ['errors' => $validator->errors()->all()]);
         }
-        $url = parse_url($request['url'], PHP_URL_HOST);
-        $domain = Domains::firstOrCreate(['name' => $url]);
-        $domain->touch();
+
+        $url = $request['url'];
+        try {
+            $response = $this->client->get($url);
+        } catch (\Exception $e) {
+            return view('home', ['errors' => ["Something was wrong within request to $url"]]);
+        }
+
+        $pageBody = $response->getBody();
+        $responseCode = $response->getStatusCode();
+        $headers = $response->getHeader('Content-Length');
+        $contentLength = empty($headers) ? null : $headers[0];
+
+
+        $domainName = parse_url($url, PHP_URL_HOST);
+        $domain = Domains::updateOrCreate(['name' => $domainName], [
+            'page_body' => $pageBody,
+            'response_code' => $responseCode,
+            'content_length' => $contentLength
+        ]);
         return redirect()->route('domains.show', ['id' => $domain->id]);
     }
 }
