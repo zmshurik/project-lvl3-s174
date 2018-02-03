1326264cc860e25b39fd4948ac73865b3e5f64b3
Alexander Kurakov <zmshurik@yandex.ru>
2018-02-03 17:41:43 +0700

    add DB migration

---
+++ database/migrations/2018_02_03_103918_add_more_columns_to_domains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMoreColumnsToDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            $table->text('main_header')->nullable();
+            $table->text('meta_keywords')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domains', function (Blueprint $table) {
+            //
+        });
+    }
+}
